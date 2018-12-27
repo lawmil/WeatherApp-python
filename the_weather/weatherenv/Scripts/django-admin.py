0f86b3bdb15c08d8f21d8b3cf91bbca3387d7647
Lawrence Milton <milton.lawrence44@gmail.com>
2018-12-27 18:48:33 -0500

    First Commit Python

---
+++ the_weather/weatherenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\milto\documents\major.projects\weather_app\the_weather\weatherenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
